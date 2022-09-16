e5bedba1df183e26f13435e8117c620967da4610
Jenna Allen <jenna.allen@vouch.us>
2022-09-16 14:40:07 -0700

    fix names with spaces

--- macros/object_pivot.sql
+++ macros/object_pivot.sql
@@ -1,11 +1,13 @@
 {%- macro object_pivot(t, c, primitive=true, include_columns=[], exclude_keys=['null']) -%}
-    {%- set query = "select distinct regexp_replace(key, '[ .]', '_') from " ~ t ~ ", lateral flatten(" ~ c ~ ")" -%}
+    {%- set query = "select distinct key, regexp_replace(key, '[ .]', '_') as alias_key from " ~ t ~ ", lateral flatten(" ~ c ~ ")" -%}
     {%- set keys_query = run_query(query) -%}
     
     {%- if execute -%}
     {%- set keys = keys_query.columns[0].values() -%}
+    {%- set alias_keys = keys_query.columns[1].values() -%}
     {%- else -%}
     {%- set keys = [] -%}
+    {%- set alias_keys = [] -%}
     {%- endif -%}
 
     select
@@ -13,11 +15,12 @@
         {{ ic }},
     {%- endfor -%}
     {%- for k in keys -%}
+    {%- set alias_key = alias_keys[loop.index0] -%}
         {%- if k not in exclude_keys -%}
             {%- if primitive -%}
-                {{ dbt_variant_utils.as_primitive(t, "get(" ~ c ~ ", '" ~ k ~ "')") }} as {{ k }}
+                {{ dbt_variant_utils.as_primitive(t, "get(" ~ c ~ ", '" ~ k ~ "')") }} as {{ alias_key }}
             {%- else -%}
-                get({{ c }}, '{{ k }}') as {{ k }}
+                get({{ c }}, '{{ k }}') as {{ alias_key }}
             {%- endif -%}
             {%- if not loop.last -%},{%- endif -%}
         {%- endif -%}
