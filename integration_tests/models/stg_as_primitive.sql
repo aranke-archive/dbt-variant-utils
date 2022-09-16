43e7a51bcd5abe5d4d8562dad3c8c777d09c6531
Jenna Allen <jenna.allen@vouch.us>
2022-09-16 13:49:56 -0700

    [DE-2366] fix decimal scale

--- integration_tests/models/stg_as_primitive.sql
+++ integration_tests/models/stg_as_primitive.sql
@@ -1,14 +1,17 @@
 select
     1::variant as same_type,
     'foo'::variant as with_null,
-    true::variant as different_types
+    true::variant as different_types,
+    0.567453454::variant as decimal_number
 union all
 select
     2::variant as same_type,
     null::variant as with_null,
-    4::variant as different_types
+    4::variant as different_types,
+    1.05::variant as decimal_number
 union all
 select
     3::variant as same_type,
     'bar'::variant as with_null,
-    false::variant as different_types
+    false::variant as different_types,
+    0.4532::variant as decimal_number
--- macros/object_pivot.sql
+++ macros/object_pivot.sql
@@ -1,26 +1,26 @@
-{% macro object_pivot(t, c, primitive=true, include_columns=[], exclude_keys=['null']) %}
-    {% set query = "select distinct regexp_replace(key, '[ .]', '_') from " ~ t ~ ", lateral flatten(" ~ c ~ ")" %}
-    {% set keys_query = run_query(query) %}
+{%- macro object_pivot(t, c, primitive=true, include_columns=[], exclude_keys=['null']) -%}
+    {%- set query = "select distinct regexp_replace(key, '[ .]', '_') from " ~ t ~ ", lateral flatten(" ~ c ~ ")" -%}
+    {%- set keys_query = run_query(query) -%}
     
-    {% if execute %}
-    {% set keys = keys_query.columns[0].values() %}
-    {% else %}
-    {% set keys = [] %}
-    {% endif %}
+    {%- if execute -%}
+    {%- set keys = keys_query.columns[0].values() -%}
+    {%- else -%}
+    {%- set keys = [] -%}
+    {%- endif -%}
 
     select
-    {% for ic in include_columns %}
+    {%- for ic in include_columns %}
         {{ ic }},
-    {% endfor %}
-    {% for k in keys %}
-        {% if k not in exclude_keys %}
-            {% if primitive %}
+    {%- endfor -%}
+    {%- for k in keys -%}
+        {%- if k not in exclude_keys -%}
+            {%- if primitive -%}
                 {{ dbt_variant_utils.as_primitive(t, "get(" ~ c ~ ", '" ~ k ~ "')") }} as {{ k }}
-            {% else %}
+            {%- else -%}
                 get({{ c }}, '{{ k }}') as {{ k }}
-            {% endif %}
-            {% if not loop.last %},{% endif %}
-        {% endif %}
-    {% endfor %}
+            {%- endif -%}
+            {%- if not loop.last -%},{%- endif -%}
+        {%- endif -%}
+    {%- endfor %}
     from {{ t }}
 {% endmacro %}
